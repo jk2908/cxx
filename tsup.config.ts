de8c25f9d35fe70a3a53bd776b7ba70d2223abbf
Jerome Kenway <jerome@digitalmarmalade.co.uk>
2024-08-19 22:20:22 +0100

    prefix node imports

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,5 +1,5 @@
-import { resolve } from 'path'
-import { env } from 'process'
+import { resolve } from 'node:path'
+import { env } from 'node:process'
 import type { Options } from 'tsup'
 
 export const tsup: Options = {
