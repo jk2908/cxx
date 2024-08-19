f430234e768cb665dd8bcd02917bf80697f8ec3a
Jerome Kenway <jerome@digitalmarmalade.co.uk>
2024-08-19 17:39:26 +0100

    fix: index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,8 +1,1 @@
-import type React from 'react'
-
-declare module 'react' {
-  interface StyleHTMLAttributes<T> extends React.HTMLAttributes<T> {
-    href?: string;
-    precedence?: 'low' | 'medium' | 'high';
-  }
-}
+export * from './dist/index.mjs'
