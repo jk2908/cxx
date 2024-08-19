1d9ea3c02d59b399165ee79ab404f392a14de1fe
Jerome Kenway <jerome@digitalmarmalade.co.uk>
2024-08-19 22:31:31 +0100

    update config to build next dir

--- index.d.ts
+++ index.d.ts
@@ -1,1 +1,2 @@
-export * from './dist/index.mjs'
+export * from './dist/index.js'
+export * from './dist/next/index.js'
--- tsup.config.ts
+++ tsup.config.ts
@@ -3,13 +3,13 @@ import { env } from 'node:process'
 import type { Options } from 'tsup'
 
 export const tsup: Options = {
-  clean: true,
-  dts: true,
-  entryPoints: [resolve(__dirname, 'src', 'index.ts')],
-  minify: env.NODE_ENV === 'production',
-  sourcemap: true,
-  splitting: true,
-  format: ['esm'],
-  target: ['esnext'],
-  external: ['react', 'react-dom', 'next'],
+	clean: true,
+	dts: true,
+	entry: ['src/index.ts', 'src/next/index.ts'],
+	minify: env.NODE_ENV === 'production',
+	sourcemap: true,
+	splitting: false,
+	format: ['esm'],
+	target: ['esnext'],
+	external: ['react', 'react-dom', 'next'],
 }
