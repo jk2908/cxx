0566e352537e20e78e183113a69d84ee312f4805
Jerome Kenway <jerome@digitalmarmalade.co.uk>
2024-08-24 22:05:22 +0100

    simplify

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { cyrb53 } from "./$"
+import { cyrb53 } from "./cyrb53"
 
 declare module 'react' {
 	interface StyleHTMLAttributes<T> extends React.HTMLAttributes<T> {
@@ -14,28 +14,28 @@ export const cxx = (_: TemplateStringsArray): readonly [Record<string, string>, 
 ]
 
 const $id = (cls: string, path: string) =>
-	`${cls}_${cyrb53(`${cls}_${path.replace(/\\/g, '/').split('/').pop()?.replace(/\./, '_')}`)}`
+	`cxx-${cyrb53(`${cls}${path.replace(/\.[^/.]+$/, '')}`)}`
+
+const varMatch = /(?:const|var|let)\s*\[(\w+),\s*(\w+),\s*(\w+)\]\s*=\s*cxx\s*`([\s\S]*?)`/gm
+const clsMatch = /\.([a-zA-Z][a-zA-Z0-9]*)\s*{/g
 
 export function inject(source: string, path: string) {
-	const varMatch = /(?:const|var|let)\s*\[(\w+),\s*(\w+),\s*(\w+)\]\s*=\s*cxx\s*`([\s\S]*?)`/gm
-	const clsMatch = /\.([a-zA-Z][a-zA-Z0-9]*)\s*{/g
-
-	const map = new Map()
+	const clsMap = new Map()
 
 	const code = source.replace(varMatch, (_: string, ...args: string[]) => {
-			const [varOne, varTwo, varThree, tmpl] = args
+		const [varOne, varTwo, varThree, tmpl] = args
 
-			const css = tmpl.replace(clsMatch, (_: string, cls: string) => {
-					const id = $id(cls, path)
-					map.set(cls, id)
+		const css = tmpl.replace(clsMatch, (_: string, cls: string) => {
+			const id = $id(cls, path)
+			clsMap.set(cls, id)
 
-					return `.${id} {`
-			})
+			return `.${id} {`
+		})
 
-			const classes = Object.fromEntries(map)
-			const href = cyrb53([...Object.keys(classes), path].join(' '))
+		const classes = Object.fromEntries(clsMap)
+		const href = cyrb53([...Object.keys(classes), path].join(''))
 
-			return `const ${varOne} = ${JSON.stringify(classes)}\nconst ${varTwo} = \`${css}\`\nconst ${varThree} = \`${href}\``
+		return `const ${varOne} = ${JSON.stringify(classes)}\nconst ${varTwo} = \`${css}\`\nconst ${varThree} = \`${href}\``
 	})
 
 	return code
