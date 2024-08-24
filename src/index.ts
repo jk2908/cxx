19c653ccc9ea6f338c5fea41aee5e3e1033dc0ba
Jerome Kenway <jerome@digitalmarmalade.co.uk>
2024-08-24 23:22:15 +0100

    inline id generation

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { cyrb53 } from "./cyrb53"
+import { cyrb53 } from './cyrb53'
 
 declare module 'react' {
 	interface StyleHTMLAttributes<T> extends React.HTMLAttributes<T> {
@@ -8,13 +8,10 @@ declare module 'react' {
 }
 
 export const cxx = (_: TemplateStringsArray): readonly [Record<string, string>, '', ''] => [
-  {},
-  '',
-  '',
+	{},
+	'',
+	'',
 ]
-
-const $id = (cls: string, path: string) =>
-	`cxx-${cyrb53(`${cls}${path.replace(/\.[^/.]+$/, '')}`)}`
 
 const varMatch = /(?:const|var|let)\s*\[(\w+),\s*(\w+),\s*(\w+)\]\s*=\s*cxx\s*`([\s\S]*?)`/gm
 const clsMatch = /\.([a-zA-Z][a-zA-Z0-9]*)\s*{/g
@@ -26,7 +23,7 @@ export function inject(source: string, path: string) {
 		const [varOne, varTwo, varThree, tmpl] = args
 
 		const css = tmpl.replace(clsMatch, (_: string, cls: string) => {
-			const id = $id(cls, path)
+			const id = `cxx-${cyrb53(`${cls}${path.replace(/\.[^/.]+$/, '')}`)}`
 			clsMap.set(cls, id)
 
 			return `.${id} {`
