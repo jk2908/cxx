bde5c2d10f577dbef8cefb2115686127ff816ec1
Jerome Kenway <jerome@digitalmarmalade.co.uk>
2024-08-27 19:20:02 +0100

    fix vite plugin path

--- examples/vite/vite.config.ts
+++ examples/vite/vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-//import cxx from '@jk2908/cxx/vite/plugin'
+import cxx from '@jk2908/cxx/vite-plugin-cxx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), cxx()],
 })
